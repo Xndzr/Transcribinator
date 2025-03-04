cc552013e8e2a01a1b05e1116d95bc29754f44b6
Xndzr <150867437+Xndzr@users.noreply.github.com>
2025-03-04 07:33:48 +0000

    git commit

--- build_mac_app.sh
+++ build_mac_app.sh
@@ -5,6 +5,6 @@ pip install -r requirements.txt
 pip install pyinstaller
 
 # Run PyInstaller to create a Mac app
-pyinstaller --onefile --windowed --name "Transcribinator" run_transcription.py
+pyinstaller --onefile --windowed --name "Transcribinator" video_transcription_GUI.py
 
 echo "Mac App created! Check the 'dist' folder."
